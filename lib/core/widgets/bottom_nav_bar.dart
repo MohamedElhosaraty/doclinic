a621415870d57f73bd8ea8e55d53ba5f74a8e2da
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2025-06-19 16:06:22 +0300

    create folder widgets in core

---
+++ lib/core/widgets/bottom_nav_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../theming/app_colors.dart';
+import '../theming/app_text_styles.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const MyBottomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.rect, width: 2),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(20.r), topRight: Radius.circular(20.r)),
+      ),
+      child: BottomNavigationBar(
+        iconSize: 22,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('Assets.svgHomeIcon'),
+            activeIcon: SvgPicture.asset(
+              'Assets.svgActiveHomeIcon,'
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset('Assets.svgActiveCartIcon'),
+            icon: SvgPicture.asset('Assets.svgCartIcon'),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('Assets.svgFavouriteIcon'),
+            activeIcon: SvgPicture.asset('Assets.svgActiveFavouriteIcon'),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon:  SvgPicture.asset('Assets.svgMenuIcon'),
+            label: "Menu",
+          ),
+        ],
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: AppColors.grayScale,
+        selectedLabelStyle: AppTextStyles.poppins12Medium(context),
+        unselectedLabelStyle: AppTextStyles.poppins12Medium(context),
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+        backgroundColor: AppColors.transparent,
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
